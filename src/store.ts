f1d4804dc348bd418f543c1561b4ec3adbba45f6
unknown <abhinaykatta97@gmail.com>
2024-04-08 00:08:19 +0530

    converted fetchallproducts async function to middleware

--- src/store.ts
+++ src/store.ts
@@ -7,6 +7,6 @@ import {
 
 import { thunk } from "redux-thunk";
 import reducers from "./redux/reducers";
-
+// ignore the warnings etc, this still works as expected,
 const store = createStore(reducers, applyMiddleware(thunk));
 export default store;
