66635bb6ebde7aa69a87f579292ec86bdaaf567c
unknown <abhinaykatta97@gmail.com>
2024-04-09 12:57:39 +0530

    added better css, sonner toast, increased performance using useMemo, useCallback hooks, added some more data in detail view and list view

--- src/store.ts
+++ src/store.ts
@@ -3,5 +3,6 @@ import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import { thunk } from "redux-thunk";
 import reducers from "./redux/reducers";
 // ignore the warnings etc, this still works as expected,
+// @ts-ignore
 const store = createStore(reducers, applyMiddleware(thunk));
 export default store;
