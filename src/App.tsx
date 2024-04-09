f29ec1a7db5434da7cb73cea96c75a56e6e92aa8
unknown <abhinaykatta97@gmail.com>
2024-04-09 12:10:18 +0530

    added better css, sonner toast, increased performance using useMemo, useCallback hooks, added some more data in detail view and list view

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import ProductList from "./components/ProductList";
 import ProductDetail from "./components/ProductDetail";
 import Cart from "./components/cart";
-import { useState } from "react";
 import Header from "./components/Header";
 
 const App = () => {
-  const [showCart, setShowCart] = useState(false);
-
   return (
     <div className="w-auto h-full min-h-screen flex flex-col bg-slate-300">
       <Router>
